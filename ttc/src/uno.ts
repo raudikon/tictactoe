fed4ab4df73343acb1ec5c0127fede4d7de37621
raudikon <raudikon@gmail.com>
2025-09-22 16:18:56 -0400

    Fully functioning ttc game with reset; no CSS.

---
+++ ttc/src/uno.ts
@@ -0,0 +1,30 @@
+
+
+type Deck = Card[]
+type Pile = Card[]
+type Hand = Card[]
+
+type PlayerID = string
+
+type Player = {
+    id: PlayerID
+    hand: Hand,
+}
+
+type Game = {
+    deck: Deck,
+    pile: Pile,
+    players: Player[]
+    currentPlayer: PlayerID
+}
+
+type Card = ??
+
+
+const sampleUno = {
+    deck: [{
+        color: 'blue',
+        value: '8',
+        type: 'number'
+    }, 'r8', '+4w', 'w', 'rreverse', 'breverse']
+}
